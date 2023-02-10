941612fce3d37de84a0aa5386f6a658e08d3046c
Modena <Modena2311@gmail.com>
2023-02-10 17:06:52 +0300

    Random

---
+++ Ramdom/Program.cs
@@ -0,0 +1,11 @@
+﻿int numerA = new Random().Next (1, 10);
+
+Console.WriteLine(numerA);
+
+int numberB = new Random().Next(1, 10);
+
+Console.WriteLine(numberB);
+
+int result = numerA + numberB;
+
+Console.WriteLine(result);
