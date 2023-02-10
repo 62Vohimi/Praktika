47652721523eff3cb901df2332190af90de42515
Modena <Modena2311@gmail.com>
2023-02-10 18:19:47 +0300

    ResenieZadaciGiri

---
+++ ReshenieZadaci/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 4;
+int c = 7;
+int d = 3;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > a) max = b;
+if (c > b) max = c;
+if (d > c) max = d;
+
+Console.Write("max= ");
+Console.WriteLine(max);
